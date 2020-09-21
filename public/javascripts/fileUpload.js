0e793e86c95cb097ae5f4a53d452131c718e27c3
umutsolmaz <48951476+bouncer535@users.noreply.github.com>
2020-09-22 01:40:36 +0300

    Product User Styles

--- public/javascripts/fileUpload.js
+++ public/javascripts/fileUpload.js
@@ -1,12 +1,27 @@
-FilePond.registerPlugin(
-    FilePondPluginImagePreview,
-    FilePondPluginImageResize,
-    FilePondPluginFileEncode,
-)
-FilePond.setOptions({
-    stylePanelAspectRatio: 150 / 100,
-    imageResizeTargetWidthÇ: 100,
-    imageResizeTargetHeight: 150	
-})
+const rootStyles = window.getComputedStyle(document.documentElement)
 
-FilePond.parse(document.body);
+if(rootStyles.getPropertyValue('--product-image-width-large') != null
+    && rootStyles.getPropertyValue('--product-image-width-large') !== '') {
+    ready()
+} else {
+    document.getElementById('main-css').addEventListener('load', ready)
+}
+
+function ready() {
+    const imageWidth = parseFloat(rootStyles.getPropertyValue('--product-image-width-large'))
+    const imageAspectRatio = parseFloat(rootStyles.getPropertyValue('--product-image-aspect-ratio'))
+    const imageHeight = imageWidth / imageAspectRatio
+    FilePond.registerPlugin(
+        FilePondPluginImagePreview,
+        FilePondPluginImageResize,
+        FilePondPluginFileEncode,
+    )
+    FilePond.setOptions({
+        stylePanelAspectRatio: 1 / imageAspectRatio,
+        imageResizeTargetWidth: imageWidth,
+        imageResizeTargetHeight: imageHeight	
+    })
+    
+    FilePond.parse(document.body);
+}
+
